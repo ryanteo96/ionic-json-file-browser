ac30a26383c20bd21680e45f9283968e4c0ab9f7
ryanteo96 <ryanteo1996@gmail.com>
2018-07-12 12:22:58 +0800

    Fix mobile bugs.

--- src/components/file-browser-list/file-browser-list-mobile.component.ts
+++ src/components/file-browser-list/file-browser-list-mobile.component.ts
@@ -8,28 +8,28 @@ import { GetNode } from "../../states/file-browser.actions";
 import { FileBrowserActionSheetMobileComponent } from "../file-browser-action-sheet/file-browser-action-sheet-mobile.component";
 
 @Component({
-  selector: "file-browser-list-mobile",
-  templateUrl: "file-browser-list.html",
-  styleUrls: ["file-browser-list.scss"],
-  providers: [FileBrowserActionSheetMobileComponent]
+	selector: "file-browser-list-mobile",
+	templateUrl: "file-browser-list-mobile.html",
+	styleUrls: ["file-browser-list.scss"],
+	providers: [FileBrowserActionSheetMobileComponent]
 })
 export class FileBrowserListMobileComponent implements OnInit {
-  @Select(FileBrowserState.getChildEntities) nodes$: Observable<Entity>;
+	@Select(FileBrowserState.getChildNodes) nodes$: Observable<Entity>;
 
-  constructor(
-    public store: Store,
-    public fileBrowser: FileBrowser,
-    public fileBrowserActionSheet: FileBrowserActionSheetMobileComponent
-  ) {}
+	constructor(
+		public store: Store,
+		public fileBrowser: FileBrowser,
+		public fileBrowserActionSheet: FileBrowserActionSheetMobileComponent
+	) {}
 
-  ngOnInit() {}
+	ngOnInit() {}
 
-  showNode(node) {
-    if (node.type === "folder") this.store.dispatch(new GetNode(node.id));
-  }
+	showNode(node) {
+		if (node.type === "folder") this.store.dispatch(new GetNode(node.id));
+	}
 
-  showActionSheet(node) {
-    event.stopPropagation();
-    this.fileBrowserActionSheet.presentActionSheet(node);
-  }
+	showActionSheet(node) {
+		event.stopPropagation();
+		this.fileBrowserActionSheet.presentActionSheet(node);
+	}
 }
